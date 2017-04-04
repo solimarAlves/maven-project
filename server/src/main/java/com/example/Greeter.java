dd550d0364ae0012fa1f7217d799754f1d280999
solimar alves dos santos <solimar@MacBook-Pro-de-solimar.local>
2017-04-04 20:27:32 -0300

    fix the checkstyle problems

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -5,19 +5,19 @@ package com.example;
  */
 public class Greeter {
 
-  /**
-   * This is a constructor.
-   */
-  public Greeter() {
+    /**
+     * This is a constructor.
+     */
+    public Greeter() {
 
-  }
+    }
 
-  /**
-   * 
-   * @param someone someone
-   * @return string
-   */
-  public final String greet(final String someone) {
-    return String.format("Hello, %s!", someone);
-  }
+    /**
+     *
+     * @param someone someone
+     * @return string
+     */
+    public final String greet(final String someone) {
+        return String.format("Hello, %s!", someone);
+    }
 }
